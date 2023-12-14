df67c359425c1b4924f5ecaef8d10d0d5e2ca943
Nikhil0592 <Aroranikhil593@gmail.com>
2023-12-14 18:14:45 +0530

    COmmit63

---
+++ Object Repository/EcommOfferPageObjects/Tomaoto_Text.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Tomaoto_Text</name>
+   <tag></tag>
+   <elementGuidId>79a0ecf0-c4c0-4d83-a3f4-022c130cd3f5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h4[@class='product-name']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/RahulShettyPracticePage/webTable2.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>webTable2</name>
+   <tag></tag>
+   <elementGuidId>3de3b8d0-6656-4542-92b0-b0b8b82e2693</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
